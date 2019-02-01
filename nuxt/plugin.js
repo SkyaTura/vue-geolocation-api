c1dedcbd098da69b29ea7c009da8368465606240
Antonio Eduardo <antonio@skyatura.com.br>
2019-02-01 10:20:37 -0200

    🐛 Fix store usage on Nuxt

--- nuxt/plugin.js
+++ nuxt/plugin.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import VueGeolocationApi from 'vue-geolocation-api'
 
-// eslint-disable-next-line
-Vue.use(VueGeolocationApi, <%= JSON.stringify(options, null, 2) %>)
+export default (ctx, inject) => {
+  // eslint-disable-next-line
+  const Component = VGA.VueGeolocationApi(Vue, <%= JSON.stringify(options, null, 2) %>)
+  const geolocation = new Component()
+  ctx.app.$geolocation = geolocation
+  inject('geolocation', geolocation)
+}
